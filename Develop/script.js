9bf1f34be64a579024667d24c2aa235d01e08c5b
Dan <bmdmartinez@gmail.com>
2021-06-21 15:54:19 -0400

    Refactored/changed up code for simplicity can now edit task fields save button works and adds to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,35 @@
 var currentDay = document.getElementById("currentDay");
 
-// var currentTime = k;
 var day = moment().format("dddd, MMMM Do");
 console.log(day);
 currentDay.textContent= day;
 
-var checkTime = function() {
-    currentTime = moment().format("hh:mm:ss")
-    console.log(currentTime);
-};
- checkTime();
+$(".saveBtn").on("click", function(){
+    console.log(this);
+    var task = $(this).siblings(".task-field").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, task);
+})
+
+$("#9am .task-field").val(localStorage.getItem("9am"));
+$("#10am .task-field").val(localStorage.getItem("10am"));
+$("#11am .task-field").val(localStorage.getItem("11am"));
+$("#12pm .task-field").val(localStorage.getItem("12pm"));
+$("#1pm .task-field").val(localStorage.getItem("1pm"));
+$("#2pm .task-field").val(localStorage.getItem("2pm"));
+$("#3pm .task-field").val(localStorage.getItem("3pm"));
+$("#4pm .task-field").val(localStorage.getItem("4pm"));
+$("#5pm .task-field").val(localStorage.getItem("5pm"));
+
+$(".task-field").on("click", "p", function(){
+    // retrieve current text (if any) on the p element
+    var taskText = $(this)
+    .text()
+    .trim();
+    console.log(taskText);
+
+    // replace the text with an input field
+    var taskInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(taskInput);
+    taskIntput.trigger("focus");
+});
