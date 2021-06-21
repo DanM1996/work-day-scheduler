6fd301829262d13f5b7df6002b2951349b6379a9
Dan <bmdmartinez@gmail.com>
2021-06-21 13:28:58 -0400

    Minor changes to change branches

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 var currentDay = document.getElementById("currentDay");
 
+// var currentTime = k;
 var day = moment().format("dddd, MMMM Do");
 console.log(day);
 currentDay.textContent= day;
+
+var checkTime = function() {
+    currentTime = moment().format("hh:mm:ss")
+    console.log(currentTime);
+};
+ checkTime();
